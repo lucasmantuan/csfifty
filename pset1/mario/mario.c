0e5f9096df4832ef7e9ed82e973de3e116cb22dc
Lucas Mantuan <lucasmantuan@outlook.com>
2022-03-20 03:15:47 +0000

    Sun, Mar 20, 2022, 3:15 AM +00:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -4,14 +4,13 @@
 int main(void)
 {
     int n = 8;
-//    for (int j = 0; j < n; j++)
-//    {
+    for (int j = 0; j < n; j++)
+    {
         for (int i = 0; i < n; i++)
         {
-            printf(".");
-            n--;
+            printf("#");
         }
-        printf("#");
+        n--;
         printf("\n");
-//    }
+    }
 }
