20e81ac8ab92f5a875a63ee5c54d7e333681feda
Lucas Mantuan <lucasmantuan@outlook.com>
2022-03-20 03:32:11 +0000

    Sun, Mar 20, 2022, 3:32 AM +00:00

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int print_space(int n);
+
 int main(void)
 {
+
+    print_space(3);
+
+/*
     int l = 8;
     int m = 2;
 //    for (int i = 0; i < l; i++)
 //    {
         for (int j = 0; j < m; j++)
         {
-            for (int k = 0; k < l - 1; k++)
-            {
-                printf(".");
-            }
+
             printf("#");
         }
 //        m++;
             printf("\n");
-//    }
+//    } */
 }
 
-int print_space(void)
+void print_space(int n)
 {
-
+    for (int i = 0; i < n; i++)
+    {
+        printf(" ");
+    }
 }
