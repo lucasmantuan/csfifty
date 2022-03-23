d16a6ad6e36ec3bb5fc6d33283a3d58d417b729a
Lucas Mantuan <lucasmantuan@outlook.com>
2022-03-23 02:01:52 +0000

    Wed, Mar 23, 2022, 2:01 AM +00:00

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -11,30 +11,27 @@ int main(void)
     int moedas = 0;
 
     // faz a contagem da quantidade de moedas
-    int troco é o troco;
-    int j  é as moedas
-
     while ((troco != 0))
     {
         if (troco >= 25)
         {
-            troco = troco - 25;
-            j = j + 1;
+            troco -= 25;
+            moedas++;
         }
         else if (troco < 25 && troco >= 10)
         {
-            troco = troco - 10;
-            j = j + 1;
+            troco -= 10;
+            moedas++;
         }
         else if (troco < 10 && troco >= 5)
         {
-            troco = troco - 5;
-            j = j + 1;
+            troco -= 5;
+            moedas++;
         }
         else if (troco < 5 && troco >= 1)
         {
-            troco = troco - 1;
-            j = j + 1;
+            troco -= 1;
+            moedas++;
         }
     }
 
