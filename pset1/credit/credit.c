a4b1cf676ac55d82e59eabb12cfd6b8f434c4a15
Lucas Mantuan <lucasmantuan@outlook.com>
2022-03-24 01:34:33 +0000

    Thu, Mar 24, 2022, 1:34 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -3,18 +3,30 @@
 
 int main (void)
 {
-    long card = 2323232323;
+    long card = 1234567890;
 
     int a;
     int x = 10;
     int y = 1;
+    int p;
 
-    for (int i = 0; i < 9; i += 2 )
+    for (int i = 0; i < 9; i++)
     {
+        p = i % 2;
+
+        if (p == 0)
+        {
+            printf("par\n");
+        }
+        else
+        {
+            printf("impar\n");
+        }
+
         a = (card % x) / y;
         x *= 10;
         y *= 10;
-        printf("%i ", a);
+        printf("%i\n", a);
     }
 
 }
