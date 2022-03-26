77875cbd5b0058a0bd17f2891a7be8ee250c1b77
Lucas Mantuan <lucasmantuan@outlook.com>
2022-03-26 03:46:31 +0000

    Sat, Mar 26, 2022, 3:46 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,61 +1,60 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int somar(int);
+int somar_digitos(int n);
+int somar_multiplicar_digitos(long n);
+int contar_digitos(long n);
 
 int main (void)
 {
-    long card = 4003600000000014;
-
-    long a;
-    int b;
-    long x = 10;
-    long y = 1;
-    int p;
-
-    int acc = 0;
-    int res;
-
-    for (int i = 0; i < 16; i++)
-    {
-        p = i % 2;
-
-        a = (card % x) / y;
-        x *= 10;
-        y *= 10;
-
-        printf("a - %li \n", a);
-
-
-        if (p != 0) {
-
-            b = a * 2;
-            if (b >= 10) {
-                res = somar(b);
-                acc += res;
-            } else {
-                acc += b;
-                //printf("b %i \n", b);
-            }
-            //printf("%i\n", acc);
-        } else {
-            acc += a;
-            //printf("a %i \n", a);
-        }
-
-        //printf("%i\n", a);
-    }
-        printf("acc %i \n", acc);
+    int resultado = somar_multiplicar_digitos(4003600000000014);
+    printf("%i\n", resultado);
 }
 
-int somar(int n)
+// Soma e multiplica os dígitos
+int somar_multiplicar_digitos(long n)
+{
+    int d = contar_digitos(n);
+    int posicao;
+    int multiplicacao = 0;
+    int soma = 0;
+    long x = 10;
+    long y = 1;
+    long z;
+
+    for (int i = 0; i < d; i++)
+    {
+        z = (n % x) / y;
+        x *= 10;
+        y *= 10;
+        posicao = i % 2;
+
+        if (posicao != 0) {
+            multiplicacao = z * 2;
+
+            if (multiplicacao >= 10) {
+                soma += somar_digitos(multiplicacao);
+            } else {
+                soma += multiplicacao;
+            }
+        } else {
+            soma += z;
+        }
+    }
+
+    return soma;
+}
+
+// Soma os dígitos internos
+int somar_digitos(int n)
 {
     int soma = 0;
+    int d = contar_digitos(n);
     int x = 10;
     int y = 1;
     int z;
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < d; i++)
     {
         z = (n % x) / y;
         x *= 10;
@@ -64,4 +63,19 @@ int somar(int n)
     }
 
     return soma;
+}
+
+// Conta a quantidade de digitos
+int contar_digitos(long n)
+{
+    int digitos = 0;
+
+    do
+    {
+        n /= 10;
+        digitos++;
+    }
+    while (n >= 1);
+
+    return digitos;
 }
