137d59c9aae1a3b7ce8c280ed79a3c88a2519ffb
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-04 21:30:33 +0000

    Mon, Apr 4, 2022, 9:30 PM +00:00

--- pset1/exercicios/credit/credit.c
+++ pset1/exercicios/credit/credit.c
@@ -1,103 +1,114 @@
-#include <cs50.h>
 #include <stdio.h>
-#include <math.h>
+#include <cs50.h>
 
-    // AMERICAN 15 DIGITOS, INICIO 34 OU 37
-    // VISA 13 OU 16 DIGITOS, INICIO 4
-    // MASTER 16 DIGITOS, INICIO 51, 52, 53, 54 OU 55
+int somar_digitos(int n);
+string somar_multiplicar_digitos(long n);
+int contar_digitos(long n);
 
-bool checksum(long number, int len)
+int main (void)
 {
-    int sum1 = 0, sum2 = 0;
-    for (int i=0; i < len; i++)
-    {
-
-        int digit = number % 10; //checar numero
-        number /= 10; //tira ultima casa
-
-        if (i % 2 == 0) //pares
-        {
-            sum2 += digit;
-        }
-        else //impares
-        {
-            digit *= 2;
-            if (digit > 9)
-            {
-
-                sum1 += (digit / 10) + (digit % 10);
-            }
-            else
-            {
-                sum1 += digit;
-            }
-        }
-    }
-
-    int sum = sum1 + sum2;
-
-    if (sum % 10 == 0)
-    { return true;  }
-    else
-    { return false; }
+    long cartao = get_long("Número: ");
+    string resultado = somar_multiplicar_digitos(cartao);
+    printf("%s\n", resultado);
 }
 
-int main(void)
+// Soma e multiplica os dígitos
+string somar_multiplicar_digitos(long n)
 {
-   long credit = get_long("numero sem ponto ou traço: ");
-   int len = 0;
-   long n = credit;
-   do
-   {
-       n /= 10;
-       len++;
-   }
-   while (n > 0);
+    int d = contar_digitos(n);
+    int pr;
+    int sg;
+    int posicao;
+    int multiplicacao = 0;
+    int soma = 0;
+    long x = 10;
+    long y = 1;
+    long z;
 
-   bool check = checksum(credit, len);
+    for (int i = 0; i < d; i++)
+    {
+        z = (n % x) / y;
+        x *= 10;
+        y *= 10;
+        posicao = i % 2;
 
-   //passou checksum
-   if (check)
-   {
-       int two = (credit / (long)pow(10, len - 2));
-       int one = (credit / (long)pow(10, len - 1));
-
-       // american
-       if (len == 15)
-       {
-           if (two == 34 || two == 37)
-            {
-           printf("AMEX\n");
-            }
-       else { printf("INVALID\n"); }
+        if (i + 1 == d)
+        {
+            pr = z;
         }
-        else if (len == 16) // MASTERCARD or VISA
-            {
-            if (one == 4)
-            {
-                printf("VISA\n");
-            }
-        else if (two > 50 && two < 56)
-            {
-                printf("MASTERCARD\n");
-            }
-                else    { printf("INVALID\n"); }
+        if (i + 2 == d)
+        {
+            sg = z;
         }
-        else if (len == 13) // VISA
-            {
-                if (one == 4)
-                {
-                printf("VISA\n");
-                }
-                else {printf("INVALID\n");}
+
+        if (posicao != 0) {
+            multiplicacao = z * 2;
+
+            if (multiplicacao >= 10) {
+                soma += somar_digitos(multiplicacao);
+            } else {
+                soma += multiplicacao;
             }
-        else
-            {
-            printf("INVALID\n");
-            }
-   }
+        } else {
+            soma += z;
+        }
+    }
+
+    // Faz a validação do número do cartao
+    if (((soma % 10) / 1) != 0)
+    {
+        return "INVALID";
+    }
+    else if (pr == 3 && (sg == 4 || sg == 7) && d == 15)
+    {
+        return "AMEX";
+    }
+    else if (pr == 5 && (sg == 1 || sg == 2 || sg == 3 || sg == 4 || sg == 5) && d == 16)
+    {
+        return "MASTERCARD";
+    }
+    else if (pr == 4 && (d == 13 || d == 16))
+    {
+        return "VISA";
+    }
     else
     {
-      printf("INVALID\n");
+        return "INVALID";
     }
+
+}
+
+// Soma os dígitos internos
+int somar_digitos(int n)
+{
+    int soma = 0;
+    int d = contar_digitos(n);
+    int x = 10;
+    int y = 1;
+    int z;
+
+    for (int i = 0; i < d; i++)
+    {
+        z = (n % x) / y;
+        x *= 10;
+        y *= 10;
+        soma += z;
+    }
+
+    return soma;
+}
+
+// Conta a quantidade de digitos
+int contar_digitos(long n)
+{
+    int digitos = 0;
+
+    do
+    {
+        n /= 10;
+        digitos++;
+    }
+    while (n >= 1);
+
+    return digitos;
 }
