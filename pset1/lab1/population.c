f55bdf24e21b74bfc4f217674fbb0631e4410323
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-05 01:18:42 +0000

    Tue, Apr 5, 2022, 1:18 AM +00:00

--- pset1/lab1/population.c
+++ pset1/lab1/population.c
@@ -7,15 +7,15 @@ int main(void)
   int inicial = 10;
   int final = 20;
   int anos;
+  float nascimentos;
+  float mortes;
 
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 1; i++)
   {
-    printf(i);
+    nascimentos = trunc(inicial / 3);
+    mortes = trunc(inicial / 4);
+    inicial = inicial + (int) nascimentos - (int) mortes;
+    printf("%i\n", inicial);
   }
 
-  float nascimentos = trunc(inicial / 3);
-  float mortes = trunc(inicial / 4);
-
-  inicial = inicial + (int) nascimentos - (int) mortes;
-  printf("%i\n", inicial);
 }
