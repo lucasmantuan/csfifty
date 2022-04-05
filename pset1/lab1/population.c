6ea1633843c7b49de52307c0c149e7c128bfb7d7
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-05 01:05:43 +0000

    Tue, Apr 5, 2022, 1:05 AM +00:00

--- pset1/lab1/population.c
+++ pset1/lab1/population.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
-int main void
+int main(void)
 {
   int inicial = 10;
   int final = 20;
   int anos;
-  
 
+  int nascimentos;
+  int mortes;
+
+  nascimentos = inicial / 3;
+  trunc(nascimentos);
+  printf("%i\n", nascimentos);
+
+  mortes = inicial / 3;
+  trunc(mortes);
+  printf("%i\n", mortes);
 }
