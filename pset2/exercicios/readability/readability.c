d7b4faa8b91d941e6b096d69282aa3ebb8e7e69f
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-12 02:33:39 +0000

    Tue, Apr 12, 2022, 2:33 AM +00:00

--- pset2/exercicios/readability/readability.c
+++ pset2/exercicios/readability/readability.c
@@ -4,11 +4,16 @@
 #include <string.h>
 
 int get_letters(string n);
+int get_words(string n);
 
 int main(void) {
     string text = get_string("Text: ");
+
     int n = get_letters(text);
     printf("%i\n", n);
+
+    int P = get_words(text);
+    printf("%i\n", P);
 }
 
 int get_letters(string n)
@@ -22,4 +27,17 @@ int get_letters(string n)
         }
     }
     return letters;
+}
+
+int get_words(string n)
+{
+    int words = 1;
+    for (int i = 0, t = strlen(n); i < t; i++)
+    {
+        if (isspace(n[i]))
+        {
+            words++;
+        }
+    }
+    return words;
 }
