38dd2963b201d5c283bc267ae992514157bf08f8
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-12 01:03:40 +0000

    Tue, Apr 12, 2022, 1:03 AM +00:00

--- pset2/anotacoes/scores.c
+++ pset2/anotacoes/scores.c
@@ -1,2 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+
+const int TOTAL = 3;
+
+int main(void)
+{
+    int scores[TOTAL];
+    for (int i = 0; i < TOTAL; i++)
+    {
+        socores[i] = get_int("Pontuação: ")
+    }
+
+    // imprimir a média
+    printf("Média: %f \n", (scores[0] + scores[1] + scores[2]) / TOTAL);
+}
