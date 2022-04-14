f10af7da225d162d0fd9e2da928a6c380a8ddc9f
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 23:21:45 +0000

    Thu, Apr 14, 2022, 11:21 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -8,6 +8,12 @@ int main(int argc, char *argv[])
 {
     char key = (char) argv[1][0];
 
+    // verificar a quantidade de parâmetros informados
+    if (argc > 2)
+    {
+        return printf("Usage: ./caesar key\n");
+    }
+
     // verifica se o parâmetro digitado é um número
     if (!isdigit(key))
     {
@@ -25,7 +31,7 @@ int main(int argc, char *argv[])
         if (isalpha(plaintext[i]))
         {
             // ...aplica a fórmula para conversão
-            int cipherindex = ((((int) plaintext[i] - 97) + key) % 26) + 97;
+            int cipherindex = ((((int) plaintext[i] - 97) + () key) % 26) + 97;
             printf("%c", (char) cipherindex);
         } else
         {
