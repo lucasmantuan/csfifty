3a3d605bbdd5c60bddf7164488e6bac15acfd89c
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 12:25:12 +0000

    Thu, Apr 14, 2022, 12:25 PM +00:00

--- pset2/exercicios/readability/readability.c
+++ pset2/exercicios/readability/readability.c
@@ -2,25 +2,50 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
+#include <math.h>
 
 int get_letters(string n);
 int get_words(string n);
+int get_sentences(string n);
 
-int main(void) {
+int main(void)
+{
     string text = get_string("Text: ");
 
-    int n = get_letters(text);
-    printf("%i\n", n);
+    // calcula a quantidade de letras, palavras e sentenças
+    int l = get_letters(text);
+    int w = get_words(text);
+    int s = get_sentences(text);
 
-    int P = get_words(text);
-    printf("%i\n", P);
+    // calcula a média de letras e sentenças
+    float avg_letters = (l * 100) / (float) w;
+    float avg_sentences = (s * 100) / (float) w;
+
+    // calcula o indice de coleman-liau
+    float index = 0.0588 * avg_letters - 0.296 * avg_sentences - 15.8;
+
+    // exibe o nível de leitura
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", (int) round(index));
+    }
 }
 
+// calcula a quantidade de letras
 int get_letters(string n)
 {
     int letters = 0;
     for (int i = 0, t = strlen(n); i < t; i++)
     {
+        // verifica se é uma letra
         if (isalpha(n[i]))
         {
             letters++;
@@ -29,15 +54,32 @@ int get_letters(string n)
     return letters;
 }
 
+// calcula a quantidade de palavras
 int get_words(string n)
 {
     int words = 1;
     for (int i = 0, t = strlen(n); i < t; i++)
     {
+        // verifica se é um espaço
         if (isspace(n[i]))
         {
             words++;
         }
     }
     return words;
+}
+
+// calcula a quantidade de sentenças
+int get_sentences(string n)
+{
+    int sentences = 0;
+    for (int i = 0, t = strlen(n); i < t; i++)
+    {
+        // verifica se é um ponto final, de exclamação ou interrogração
+        if (n[i] == 46 || n[i] == 63 || n[i] == 33)
+        {
+            sentences++;
+        }
+    }
+    return sentences;
 }
