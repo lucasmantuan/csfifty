ba430a1c4aa606138c6ddd8a1cf6e941f0a1122e
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 20:36:30 +0000

    Thu, Apr 14, 2022, 8:36 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -6,15 +6,11 @@ int main(void)
 {
     int key = 1;
     string plaintext = get_string("plaintext: ");
-    string ciphertext = plaintext;
 
     for (int i = 0, n = strlen(plaintext); i < n; i++ )
     {
         int cipherindex = ((((int) plaintext[i] - 97) + key) % 26) + 97;
-        printf("index     : %i\n", cipherindex);
-        ciphertext[i] += (char) cipherindex;
         printf("chipertext: %s\n", ciphertext);
     }
 
-    printf("chipertext: %s\n", ciphertext);
 }
