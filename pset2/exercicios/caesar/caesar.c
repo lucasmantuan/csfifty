0aa3b4ff3b729fa757231fc9a96bbe8f520c7c94
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 19:34:51 +0000

    Thu, Apr 14, 2022, 7:34 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -7,7 +7,7 @@ int main(void)
     int key = 1;
     //int index = (0 + key) % 26;
 
-    int ciphertext = atoi(plaintext);
+    int ciphertext = atoi(plaintext[0]);
 
     printf("%c\n", ciphertext);
 }
