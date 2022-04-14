76d50daed351144b2733676756e2792d44e0d830
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 20:29:20 +0000

    Thu, Apr 14, 2022, 8:29 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
     int key = 1;
     string plaintext = get_string("plaintext: ");
-    string chipertext = plaintext;
-    int chiperindex = ((((int) plaintext[0] - 97) + key) % 26) + 97;
+    string ciphertext = plaintext;
 
-    chipertext[0] = (char) chiperindex;
+    for (int i = 0, n = strlen(plaintext); i < n; i++ )
+    {
+        int cipherindex = ((((int) plaintext[i] - 97) + key) % 26) + 97;
+        ciphertext[i] += (char) cipherindex;
+    }
 
-    printf("%s\n", chipertext);
+    printf("chipertext: %s\n", ciphertext);
 }
