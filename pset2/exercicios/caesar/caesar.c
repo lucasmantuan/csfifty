dc281b1376fb33d83882e6cac2a85acb92bebd7c
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 21:15:14 +0000

    Thu, Apr 14, 2022, 9:15 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -6,14 +6,20 @@
 
 int main(int argc, string argv[])
 {
+    // guarda a chave informada pelo usuário
     int key = atoi(argv[1]);
+
+    // solicita o texto para conversão
     string plaintext = get_string("plaintext: ");
 
+    // para cada letra informada...
     printf("ciphertext: ");
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
+        // ...verifica se é uma letra e...
         if (isalpha(plaintext[i]))
         {
+            // ...aplica a fórmula para conversão
             int cipherindex = ((((int) plaintext[i] - 97) + key) % 26) + 97;
             printf("%c", (char) cipherindex);
         } else
