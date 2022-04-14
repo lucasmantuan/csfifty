a734f038b359c7e8bedc68748244f4664cdd1508
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 19:39:25 +0000

    Thu, Apr 14, 2022, 7:39 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -7,8 +7,8 @@ int main(void)
     string plaintext = get_string("plaintext: ");
     int key = 1;
     //int index = (0 + key) % 26;
-printf("%i\n", atoi("L"));
-    //int ciphertext = atoi(plaintext[0]);
 
-    //printf("%i\n", ciphertext);
+    int ciphertext = plaintext[0];
+
+    printf("%i\n", ciphertext);
 }
