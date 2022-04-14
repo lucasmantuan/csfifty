6457df14692b20653816e8e6484fd15f65335335
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 13:27:33 +0000

    Thu, Apr 14, 2022, 1:27 PM +00:00

--- pset2/outros/caesar.c
+++ pset2/outros/caesar.c
@@ -3,12 +3,11 @@
 #include <string.h>
 #include <cc50.h>
 
-int 
 main(int argc, char *argv[])
 {
     int k = atoi(argv[1]);
     int l = argc;
-    
+
     if (l >= 3)
     {
         printf("Por favor, tente novamente.\n");
@@ -17,11 +16,11 @@ main(int argc, char *argv[])
 
     printf("Por favor, digite o texto.\n");
     string p = GetString();
-    
+
     for (int i = 0, n = strlen(p); i < n; i++)
     {
         printf("%c", p[i]);
     }
-    
+
     return 0;
 }
