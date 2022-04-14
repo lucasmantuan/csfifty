02c682f099aba1e6eb3dc1f2288ba74db85fe39b
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-14 19:38:13 +0000

    Thu, Apr 14, 2022, 7:38 PM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <stdlib.h>
 
 int main(void)
 {
     string plaintext = get_string("plaintext: ");
     int key = 1;
     //int index = (0 + key) % 26;
+printf("%i\n", atoi("L"));
+    //int ciphertext = atoi(plaintext[0]);
 
-    int ciphertext = atoi(plaintext[0]);
-
-    printf("%c\n", ciphertext);
+    //printf("%i\n", ciphertext);
 }
