bd9a8b3ac4e35ba04debbfe151784251733946e8
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-15 03:14:07 +0000

    Fri, Apr 15, 2022, 3:14 AM +00:00

--- pset2/exercicios/caesar/caesar.c
+++ pset2/exercicios/caesar/caesar.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[])
+int main(int argc, string argv[])
 {
-    char key = argv[1];
-    printf("%c\n", key);
+    string key = argv[1];
+    printf("%s\n", key);
 
     // verificar a quantidade de parâmetros informados
     if (argc > 2)
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
     }
 
     // verifica se o parâmetro digitado é um número
-    if (!isdigit(key))
+    if (isdigit(atoi(key)))
     {
         return printf("Usage: ./caesar key\n");
     }
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
         if (isalpha(plaintext[i]))
         {
             // ...aplica a fórmula para conversão
-            int cipherindex = ((((int) plaintext[i] - 97) + (key - '0')) % 26) + 97;
+            int cipherindex = ((((int) plaintext[i] - 97) + atoi(key)) % 26) + 97;
             printf("%c", (char) cipherindex);
         }
         else
