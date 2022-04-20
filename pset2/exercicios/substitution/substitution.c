828c15868e39eee38f76a81b4078584f10e54bd2
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-20 01:36:18 +0000

    Wed, Apr 20, 2022, 1:36 AM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -4,7 +4,14 @@
 
 int main(void)
 {
-    string plaintext = get_string("String: ");
+    string plaintext = get_string("Plaintext: ");
+    string key = "wxy"
+    string ciphertext[str]
+
+    if (plaintext[0] == 'a')
+    {
+        printf("%c\n", plaintext[0]);
+    }
 
     printf("%s\n", plaintext);
 }
