e62c462e8d284187a72dabf715e2768eba141491
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-21 02:29:21 +0000

    Thu, Apr 21, 2022, 2:29 AM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -12,18 +12,27 @@ int main(void)
     // percorre 26 vezes...
     for (int j = 0; j < 26; j++)
     {
-        if (isalpha(plaintext[i]))
-        {
-
-        }
-        else
-        {
-
-        }
-
         // cada uma das letras de plaintext...
         for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
+
+            if (isalpha(plaintext[i]))
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+
+
+
+
+
+
+
             // verificando qual é a letra...
             if (plaintext[i] == character)
             {
