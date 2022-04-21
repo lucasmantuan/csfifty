052480f509fac9a2cd9e82cb0f59515d14c1e90d
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-21 02:22:33 +0000

    Thu, Apr 21, 2022, 2:22 AM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -9,7 +9,7 @@ int main(void)
     char ciphertext[strlen(plaintext)];
     int character = 97;
 
-    // percorre plaintext 26 vezes
+    // percorre 26 vezes...
     for (int j = 0; j < 26; j++)
     {
         if (isalpha(plaintext[i]))
@@ -21,9 +21,10 @@ int main(void)
 
         }
 
-        //
+        // cada uma das letras de plaintext...
         for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
+            // 
             if (plaintext[i] == character)
             {
                 ciphertext[i] = key[j];
