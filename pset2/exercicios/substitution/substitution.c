313bf28109d86d8c49570bed71421820df004089
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-22 20:43:43 +0000

    Fri, Apr 22, 2022, 8:43 PM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -6,23 +6,19 @@
 int main(void)
 {
     string key = "VcHpRzGjNtLsKfBdQwAxEuYmOi";
-
     string plaintext = get_string("plaintext: ");
-
     int size = strlen(plaintext);
-
     char ciphertext[size];
-
     int character = 97;
 
     // percorre 26 vezes cada letra
     for (int i = 0; i < 26; i++)
     {
-        if (plaintext[i] == character)
+        if (plaintext[0] == character)
         {
             printf("%i\n", character);
             // ciphertext[i] = tolower(key[0]);
-            break;
+            \break;
         }
         character++;
     }
