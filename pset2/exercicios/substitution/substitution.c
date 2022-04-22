fac1800133845fe181116548e014fc1c0b539e71
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-22 14:07:27 +0000

    Fri, Apr 22, 2022, 2:07 PM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -26,49 +26,49 @@ int main(int argc, string argv[])
     int character = 97;
 
     // percorre 26 vezes
-    // for (int j = 0; j < 26; j++)
-    // {
+    for (int j = 0; j < 26; j++)
+    {
         // cada uma das letras de plaintext...
         for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
-            // preenche o array com valores nulos
-            ciphertext[i] = ' ';
-
             // primeiro verificando se é uma letra...
             if (isalpha(plaintext[i]))
             {
-                // // depois verificando se é maiuscula e fazendo sua conversão...
-                // if (isupper(plaintext[i]))
-                // {
-                //     // para então verificar qual é a letra...
-                //     if (tolower(plaintext[i]) == character)
-                //     {
-                //         // e fazer a substituição apropriada se for uma letra...
-                //         ciphertext[i] = toupper(key[0]);
-                //     }
-                // }
-                // // entretanto se for minuscula, não faz sua conversão ...
-                // else
-                // {
-                //     //
-                //     if (plaintext[i] == character)
-                //     {
-                //         ciphertext[i] = tolower(key[0]);
-                //     }
-                // }
+                // depois verificando se é maiuscula e fazendo sua conversão...
+                if (isupper(plaintext[i]))
+                {
+                    // para então verificar qual é a letra...
+                    if (tolower(plaintext[i]) == character)
+                    {
+                        // e fazer a substituição apropriada se for uma letra...
+                        ciphertext[i] = toupper(key[0]);
+                    }
+                }
+                // entretanto se for minuscula, não faz sua conversão ...
+                else
+                {
+                    //
+                    if (plaintext[i] == character)
+                    {
+                        ciphertext[i] = tolower(key[0]);
+                    }
+                }
             }
             else
             {
                 // ou então manter o caractere.
-                printf("teste ciphertext: %c\n", ciphertext[i]);
-                printf("teste plaintext: %c\n", plaintext[i]);
                 ciphertext[i] = plaintext[i];
-                printf("teste ciphertext: %c\n", ciphertext[i]);
             }
-            // character++;
+            character++;
         }
-    // }
+    }
 
-    printf("ciphertext: %s\n", ciphertext);
+    printf("ciphertext: ");
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        printf("%c", ciphertext[i]);
+    }
+    printf("\n");
+
     return 0;
 }
