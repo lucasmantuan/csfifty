ae2e2502e7b34e8424dae8ac326f50dfedd276ce
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-22 12:57:22 +0000

    Fri, Apr 22, 2022, 12:57 PM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
@@ -65,8 +66,8 @@ int main(int argc, string argv[])
             }
 
         }
+        getchar ciphertext;
 
-        character++;
     }
 
     printf("ciphertext: %s\n", ciphertext);
