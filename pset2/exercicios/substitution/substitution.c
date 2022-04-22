14fc936dcdb01067e3577fffa280e8f16481bc07
Lucas Mantuan <lucasmantuan@outlook.com>
2022-04-22 20:52:07 +0000

    Fri, Apr 22, 2022, 8:52 PM +00:00

--- pset2/exercicios/substitution/substitution.c
+++ pset2/exercicios/substitution/substitution.c
@@ -3,26 +3,34 @@
 #include <string.h>
 #include <ctype.h>
 
+int qual_letra(char n);
+
 int main(void)
 {
     string key = "VcHpRzGjNtLsKfBdQwAxEuYmOi";
     string plaintext = get_string("plaintext: ");
     int size = strlen(plaintext);
     char ciphertext[size];
-    int character = 97;
-
-    // percorre 26 vezes cada letra
-    for (int i = 0; i < 26; i++)
-    {
-        if (plaintext[0] == character)
-        {
-            printf("%i\n", character);
-            // ciphertext[i] = tolower(key[0]);
-            \break;
-        }
-        character++;
-    }
 
     printf("%s\n", ciphertext);
     return 0;
+
+    int teste = qual_letra(plaintext[0]);
+    printf("%i é igual \n", teste);
+}
+
+// retorna qual é a letra recebida como parâmetro
+int qual_letra(char n)
+{
+    int character = 97;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (n == character)
+        {
+            // ciphertext[i] = tolower(key[0]);
+            return character;
+        }
+        character++;
+    }
 }
