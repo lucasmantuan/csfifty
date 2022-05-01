0ac74928e8d77528cd7c6a3faca8a30f5b827a32
Lucas Mantuan <lucasmantuan@outlook.com>
2022-05-01 01:34:48 +0000

    Sun, May 1, 2022, 1:34 AM +00:00

--- pset3/anotacoes/phonebook.c
+++ pset3/anotacoes/phonebook.c
@@ -4,28 +4,27 @@
 
 typedef struct
 {
-    string name;
-    string number;
+     string name;
+     string number;
 }
 person;
 
 int main(void)
 {
-    person people[2];
-    people[0].name = "Lucas";
-    people[0].number = "123-456";
-    people[1].name = "Camila";
-    people[1].number = "456-789";
+     person people[2];
+     people[0].name = "Brian";
+     people[0].number = "+1-617-495-1000";
+     people[1].name = "David";
+     people[1].number = "+1-949-468-2750";
 
-    for (int i = 0; i < 2; i++)
-    {
-        if (strcmp(people[i].name, "Camila") == 0)
-        {
-            printf("Found %s\n", people[i].number);
-            return 0;
-        }
-        printf("Not Found\n");
-        return 1;
-    }
-
+     for (int i = 0; i < 2; i++)
+     {
+         if (strcmp(people[i].name, "David") == 0)
+         {
+              printf("Encontrado %s\n", people[i].number);
+              return 0;
+         }
+     }
+     printf("Não encontrado\n");
+     return 1;
 }
