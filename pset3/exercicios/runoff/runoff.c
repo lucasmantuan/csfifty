44790dc0f103d3c4d6037fe03965edf69e79a8ce
Lucas Mantuan <lucasmantuan@outlook.com>
2022-05-21 19:56:13 +0000

    Sat, May 21, 2022, 7:56 PM +00:00

--- pset3/exercicios/runoff/runoff.c
+++ pset3/exercicios/runoff/runoff.c
@@ -146,7 +146,7 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            int index = preferences[i, j];
+            int index = preferences[i][j];
 
             if (candidates[index].eliminated == false)
             {
