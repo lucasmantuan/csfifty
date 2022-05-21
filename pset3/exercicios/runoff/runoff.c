3f4bd5e93e803b50ed9716c7be60e4baf0b3f405
Lucas Mantuan <lucasmantuan@outlook.com>
2022-05-21 19:09:31 +0000

    Sat, May 21, 2022, 7:09 PM +00:00

--- pset3/exercicios/runoff/runoff.c
+++ pset3/exercicios/runoff/runoff.c
@@ -128,7 +128,6 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-
     for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(name, candidates[i].name) == 0)
