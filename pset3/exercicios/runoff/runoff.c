3205e6ff2346825717619a737c780f27c118c77e
Lucas Mantuan <lucasmantuan@outlook.com>
2022-05-23 02:53:35 +0000

    Mon, May 23, 2022, 2:53 AM +00:00

--- pset3/exercicios/runoff/runoff.c
+++ pset3/exercicios/runoff/runoff.c
@@ -175,32 +175,18 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // int min = 0;
-    // int index = 0;
-
-    // for (int i = 0; i < voter_count; i++)
-    // {
-    //     if (!candidates[i].eliminated)
-    //     {
-    //         min = candidates[i].votes;
-    //         index = i;
-    //         break;
-    //     }
-    // }
-
-    // for (int i = index + 1; i < voter_count; i++)
-    // {
-    //     if (!candidates[i].eliminated)
-    //     {
-    //         if (candidates[i].votes < min)
-    //         {
-    //             min = candidates[i].votes;
-    //         }
-    //     }
-    // }
-    // return min;
-
     int min = 0;
+    int index = 0;
+
+    for (int i = 0; i < voter_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            min = candidates[i].votes;
+            index = i;
+            break;
+        }
+    }
 
     for (int i = 0; i < voter_count; i++)
     {
@@ -212,8 +198,23 @@ int find_min(void)
             }
         }
     }
-
+    
     return min;
+
+    // int min = 0;
+
+    // for (int i = 0; i < voter_count; i++)
+    // {
+    //     if (candidates[i].eliminated == false)
+    //     {
+    //         if (candidates[i].votes < min)
+    //         {
+    //             min = candidates[i].votes;
+    //         }
+    //     }
+    // }
+
+    // return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
