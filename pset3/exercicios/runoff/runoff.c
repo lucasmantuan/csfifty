12357af73756ea28ecaa7b39933b942d8b0e1561
Lucas Mantuan <lucasmantuan@outlook.com>
2022-05-23 02:56:15 +0000

    Mon, May 23, 2022, 2:56 AM +00:00

--- pset3/exercicios/runoff/runoff.c
+++ pset3/exercicios/runoff/runoff.c
@@ -198,23 +198,8 @@ int find_min(void)
             }
         }
     }
-    
+
     return min;
-
-    // int min = 0;
-
-    // for (int i = 0; i < voter_count; i++)
-    // {
-    //     if (candidates[i].eliminated == false)
-    //     {
-    //         if (candidates[i].votes < min)
-    //         {
-    //             min = candidates[i].votes;
-    //         }
-    //     }
-    // }
-
-    // return min;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
