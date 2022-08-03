af242a4ab85b79c93f2b1282face5474bf73a1fc
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-03 01:58:52 +0000

    Wed, Aug 3, 2022, 1:58 AM +00:00

--- pset4/anotacoes/copy.c
+++ pset4/anotacoes/copy.c
@@ -9,12 +9,23 @@ int main(void)
     char *s = get_string("s: ");
     char *t = malloc(strlen(s) + 1);
 
+    if (t == NULL)
+    {
+        return 1;
+    }
+
     for (int i = 0, n = strlen(s); i < n + 1; i++)
     {
         t[i] = s[i];
     }
 
-    t[0] = toupper(t[0]);
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    free(t);
 }
