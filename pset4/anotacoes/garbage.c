c9000233ef69ee8cb45c4dd514de0e8ac3989119
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-04 01:12:02 +0000

    Thu, Aug 4, 2022, 1:12 AM +00:00

--- pset4/anotacoes/garbage.c
+++ pset4/anotacoes/garbage.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    int *x;
-    int *y;
-    x = malloc(sizeof(int));
-    *x = 42;
-    *y = 13;
-    y = x;
-    *y = 13;
+    int scores[3];
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", scores[i]);
+    }
 }
