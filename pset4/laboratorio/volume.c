ca9b579dec12c812654d157f5a6cf8727d5f51cc
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-04 01:55:50 +0000

    Thu, Aug 4, 2022, 1:55 AM +00:00

--- pset4/laboratorio/volume.c
+++ pset4/laboratorio/volume.c
@@ -33,11 +33,15 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // Copy header from input file to output file
-
-
+     BYTE buffer;
+     while(fread(&buffer, sizeof(BYTE), 1, source))
+     {
+         fwrite(&buffer, sizeof(BYTE), 1, destination);
+     }
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
     fclose(input);
     fclose(output);
+    return 0;
 }
