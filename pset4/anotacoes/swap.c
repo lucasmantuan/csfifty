f3814730dbefa683e5e54e5e4a6779110dc38dc2
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-04 01:15:57 +0000

    Thu, Aug 4, 2022, 1:15 AM +00:00

--- pset4/anotacoes/swap.c
+++ pset4/anotacoes/swap.c
@@ -7,13 +7,13 @@ int main(void)
     int x = 1;
     int y = 2;
     printf("x is %i, y is %i\n", x, y);
-    swap(x, y);
+    swap(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    int tmp = a;
-    a = b;
-    b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
