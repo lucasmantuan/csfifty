104d173453443ca5bae17c5dd45ef812d5073aaa
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-16 19:54:49 +0000

    Tue, Aug 16, 2022, 7:54 PM +00:00

--- pset4/exercicios/filter_less/helpers.c
+++ pset4/exercicios/filter_less/helpers.c
@@ -23,6 +23,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int sepiaRed = 255;
+    int sepiaGreen = 255;
+    int sepiaBlue = 255;
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            media = round((image[j][i].rgbtRed + image[j][i].rgbtGreen + image[j][i].rgbtBlue) / 3.0);
+            image[j][i].rgbtRed = media;
+            image[j][i].rgbtGreen = media;
+            image[j][i].rgbtBlue = media;
+        }
+    }
+
     return;
 }
 
