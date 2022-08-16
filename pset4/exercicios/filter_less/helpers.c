d801b7b7cfd1c066b6f9b07c11aacebcd8f8829d
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-16 20:00:34 +0000

    Tue, Aug 16, 2022, 8:00 PM +00:00

--- pset4/exercicios/filter_less/helpers.c
+++ pset4/exercicios/filter_less/helpers.c
@@ -23,18 +23,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    int sepiaRed = 255;
-    int sepiaGreen = 255;
-    int sepiaBlue = 255;
+    int sepiaRed = 0;
+    int sepiaGreen = 0;
+    int sepiaBlue = 0;
 
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
         {
-            media = round((image[j][i].rgbtRed + image[j][i].rgbtGreen + image[j][i].rgbtBlue) / 3.0);
-            image[j][i].rgbtRed = media;
-            image[j][i].rgbtGreen = media;
-            image[j][i].rgbtBlue = media;
+            sepiaRed = round(0.393 * image[j][i].rgbtRed + 0.769 * image[j][i].rgbtGreen + 0.189 * image[j][i].rgbtBlue);
+            sepiaGreen = round(0.349 * image[j][i].rgbtRed + 0.686 * image[j][i].rgbtGreen + 0.168 * image[j][i].rgbtBlue);
+            sepiaBlue = round(0.272 * image[j][i].rgbtRed + 0.534 * image[j][i].rgbtGreen + 0.131 * image[j][i].rgbtBlue);
+
+            sepiaRed > 255 ? image[j][i].rgbtRed = 255 : image[j][i].rgbtRed = sepiaRed;
+            sepiaGreen > 255 ? image[j][i].rgbtGreen = 255 : image[j][i].rgbtGreen= sepiaGreen;
+            sepiaBlue > 255 ? image[j][i].rgbtBlue = 255 : image[j][i].rgbtBlue = sepiaBlue;
         }
     }
 
