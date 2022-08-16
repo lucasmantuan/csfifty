fef8a2bf14f50ab356fa1dffcd51fe115378f785
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-16 20:58:41 +0000

    Tue, Aug 16, 2022, 8:58 PM +00:00

--- pset4/exercicios/filter_less/helpers.c
+++ pset4/exercicios/filter_less/helpers.c
@@ -75,6 +75,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             // image[i][width - (j + 1)] = aux;
 
             aux = image[i][j];
+            image[i][j] = image[i][height - (j + 1)];
             image[i][height - (j + 1)] = aux;
         }
     }
