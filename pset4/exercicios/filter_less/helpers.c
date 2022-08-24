cc7588b4bb2327af64f5c36ee2ec3046dbcb2c49
Lucas Mantuan <lucasmantuan@outlook.com>
2022-08-24 01:11:38 +0000

    Wed, Aug 24, 2022, 1:11 AM +00:00

--- pset4/exercicios/filter_less/helpers.c
+++ pset4/exercicios/filter_less/helpers.c
@@ -82,11 +82,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 1; i < height - 1; i++)
+    for (int i = 0; i < height 0; i++)
     {
-        for (int j = 1; j < width - 1; j++)
+        for (int j = 0; j < width 0; j++)
         {
-
+            temp[i][j] = image[i][j]
         }
     }
     return;
