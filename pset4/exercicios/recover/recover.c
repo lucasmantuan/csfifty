ff73aca5cf5474f85726879737c5a2f00b5bb442
Lucas Mantuan <lucasmantuan@outlook.com>
2022-12-27 20:27:08 +0000

    Tue, Dec 27, 2022, 8:27 PM +00:00

--- pset4/exercicios/recover/recover.c
+++ pset4/exercicios/recover/recover.c
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
     // Verifica a quantidade de argumentos
     if (argc != 2)
     {
-        
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
     }
+
+    // Abre o arquivo para leitura
+    FILE *input_file = fopen(argv[1], "r");
+
+    // 
 }
