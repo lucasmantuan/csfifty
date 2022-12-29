42550eecd93725602b1f8c960ff3a76206cc691d
Lucas Mantuan <lucasmantuan@outlook.com>
2022-12-29 02:42:49 +0000

    Thu, Dec 29, 2022, 2:42 AM +00:00

--- pset5/anotacoes/listas.c
+++ pset5/anotacoes/listas.c
@@ -10,5 +10,30 @@ node;
 
 int main (void)
 {
+    node *list = NULL;
+
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 1;
+    n->next = NULL;
+
+    list = n;
+
+    n = malloc(sizeof(node))
+    if (n == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    n->number = 2;
+    n->next = NULL;
+
+    list->next = n;
+
     
 }
