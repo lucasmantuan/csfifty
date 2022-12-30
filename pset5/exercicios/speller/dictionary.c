3c864acc0a69d4b2d826cf8b8b29caff46ca1947
Lucas Mantuan <lucasmantuan@outlook.com>
2022-12-30 04:32:17 +0000

    Fri, Dec 30, 2022, 4:32 AM +00:00

--- pset5/exercicios/speller/dictionary.c
+++ pset5/exercicios/speller/dictionary.c
@@ -35,8 +35,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *file = fopen(dictionary, "r");
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
