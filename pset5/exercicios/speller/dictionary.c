569150d14ed03c0a539be106136d80609c2f371a
Lucas Mantuan <lucasmantuan@outlook.com>
2022-12-30 12:32:37 +0000

    Fri, Dec 30, 2022, 12:32 PM +00:00

--- pset5/exercicios/speller/dictionary.c
+++ pset5/exercicios/speller/dictionary.c
@@ -54,8 +54,15 @@ bool load(const char *dictionary)
             return false;
         }
 
-        
+        strcpy(n->word, word);
+        hash_value = hash(word);
+        n->next = table[hash_value];
+        table[hash_value] = n;
+        word_count++;
     }
+
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
