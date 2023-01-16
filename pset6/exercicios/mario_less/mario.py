13218c37b79048cff7eb0ed3db9296fd27e5e39a
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-16 02:38:55 +0000

    Mon, Jan 16, 2023, 2:38 AM +00:00

--- pset6/exercicios/mario_less/mario.py
+++ pset6/exercicios/mario_less/mario.py
@@ -4,3 +4,11 @@ while True:
     n = get_int("Heigth: ")
     if n > 0 and n < 9:
         break
+
+for i in range(0, n, 1):
+    for j in range(0, n 1):
+        if (i + j < n - 1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
