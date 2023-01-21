0ca9f9f8f9c5bb5fa8b9a79578edda21903e3309
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-21 03:59:21 +0000

    Sat, Jan 21, 2023, 3:59 AM +00:00

--- pset6/exercicios/cash/cash.py
+++ pset6/exercicios/cash/cash.py
@@ -1,6 +1,11 @@
 from cs50 import get_float
 
 while True:
-    cents = get_float("Chage: ")
+    cents = get_float("Change: ")
     if (cents > 0):
         break
+
+cents = round(cents * 100)
+
+count = 0
+
