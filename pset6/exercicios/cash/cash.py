d47e2afb8cd0a364eb3fd7f6145da8a22e2371a0
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-21 04:03:29 +0000

    Sat, Jan 21, 2023, 4:03 AM +00:00

--- pset6/exercicios/cash/cash.py
+++ pset6/exercicios/cash/cash.py
@@ -13,3 +13,14 @@ while cents >= 25:
     cents = cents - 25
     count += 1
 
+while cents >= 10:
+    cents = cents - 10
+    count += 1
+
+while cents >= 5:
+    cents = cents - 5
+    count += 1
+
+while cents >= 1:
+    cents = cents - 1
+    count += 1
