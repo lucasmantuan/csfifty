6cfc1816a6010e2b46e9ae1132f8c26efefddcc5
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-21 14:33:42 +0000

    Sat, Jan 21, 2023, 2:33 PM +00:00

--- pset6/exercicios/credit/credit.py
+++ pset6/exercicios/credit/credit.py
@@ -8,4 +8,12 @@ while True:
 def luhnChecksum(card):
     def digitsOf(n):
         return [int(d) for d in str(n)]
-    
+    digits = digitsOf(card)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digitsOf(d * 2))
+    return checksum % 10
+
