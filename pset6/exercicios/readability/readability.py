be53f5a04e612072bc13d16d51c4300e37202f72
Lucas Mantuan <lucasmantuan@outlook.com>
2023-01-23 02:23:59 +0000

    Mon, Jan 23, 2023, 2:23 AM +00:00

--- pset6/exercicios/readability/readability.py
+++ pset6/exercicios/readability/readability.py
@@ -17,4 +17,8 @@ for i in text:
 index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8
 
 if index < 1:
-    
+    print("Before Grade 1")
+elif >= 16:
+    print("Grade 16+")
+else:
+    print("Grade: ", round(index))
