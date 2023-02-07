af1b6e16d3cad9e5826a52247c471a66cc24713c
Lucas Mantuan <lucasmantuan@outlook.com>
2023-02-07 11:08:42 +0000

    Tue, Feb 7, 2023, 11:08 AM +00:00

--- pset6/exercicios/dna/dna.py
+++ pset6/exercicios/dna/dna.py
@@ -2,4 +2,8 @@ if len(sys.argv) != 3:
     sys.exit("python dna.py data.csv sequence.txt")
 
 database = []
-with open()
+with open(sys.argv[1], "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        database.append(row)
+
