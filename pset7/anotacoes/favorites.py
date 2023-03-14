be3bed865f72f3f1ff1d5fd9fae7d17c72c1abce
Lucas Mantuan <lucasmantuan@outlook.com>
2023-03-14 11:06:57 +0000

    Tue, Mar 14, 2023, 11:06 AM +00:00

--- pset7/anotacoes/favorites.py
+++ pset7/anotacoes/favorites.py
@@ -1,6 +1,15 @@
 import csv
 
+titles = {}
+
 with open('CS50 2019 - Lecture 7 - Favorite TV Shows (Responses).csv', 'r') as file:
     reader = csv.DictReader(file)
+
     for row in reader:
-        print(row['title'])
+        title = row['title'].strip().upper()
+        if title not in titles:
+            titles[title] = 0
+            titles[title] += 1
+
+    for title in sorted(titles):
+        print(title, titles[title])
