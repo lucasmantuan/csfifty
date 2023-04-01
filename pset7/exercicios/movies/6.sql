e5c30006b3855c3a3e04d427b58df92ff38089d1
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-01 04:08:56 +0000

    Sat, Apr 1, 2023, 4:08 AM +00:00

--- pset7/exercicios/movies/6.sql
+++ pset7/exercicios/movies/6.sql
@@ -1,7 +1,7 @@
 -- Em 6.sql, escreva uma consulta SQL para determinar a avaliação média de todos os filmes lançados em 2012.
 -- Sua consulta deve gerar uma tabela com uma única coluna e uma única linha (sem incluir o cabeçalho) contendo a classificação média.
 
-SELECT AVG(rating FROM ratings
+SELECT AVG(rating) FROM ratings
 WHERE movie_id IN (
     SELECT id FROM movies
     WHERE year = 2012
