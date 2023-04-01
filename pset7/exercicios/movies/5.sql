46657fd51d18c4346dd79bda0895ff4125b998a3
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-01 04:07:16 +0000

    Sat, Apr 1, 2023, 4:07 AM +00:00

---
+++ pset7/exercicios/movies/5.sql
@@ -0,0 +1,6 @@
+-- Em 5.sql, escreva uma consulta SQL para listar os títulos e anos de lançamento de todos os filmes de Harry Potter, em ordem cronológica.
+-- Sua consulta deve gerar uma tabela com duas colunas, uma para o título de cada filme e outra para o ano de lançamento de cada filme.
+-- Você pode presumir que o título de todos os filmes de Harry Potter começará com aspalavras “Harry Potter” e que se o título de um filme começar com as palavras “Harry Potter”, é um filme de Harry Potter.
+
+SELECT title, year FROM movies
+WHERE title LIKE '%Harry Potter%';
