aedfce41116a2e29d0e47c45ba2b46cbd3ce038e
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-01 04:46:35 +0000

    Sat, Apr 1, 2023, 4:46 AM +00:00

--- pset7/exercicios/movies/8.sql
+++ pset7/exercicios/movies/8.sql
@@ -2,5 +2,7 @@
 -- Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada pessoa.
 -- Você pode presumir que há apenas um filme no banco de dados com o título Toy Story.
 
-SELECT people.name, stars.movie_id FROM people
-INNER JOIN stars ON people.id = stars.person_id;
+SELECT people.name FROM people
+INNER JOIN stars ON people.id = stars.person_id
+INNER JOIN movies ON movies.id = stars.movie_id
+WHERE movies.title = 'Toy Story';
