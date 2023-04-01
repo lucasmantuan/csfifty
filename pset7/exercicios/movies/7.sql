946199c585cc1cc28c43cb05371683cd5b06c43a
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-01 04:14:39 +0000

    Sat, Apr 1, 2023, 4:14 AM +00:00

--- pset7/exercicios/movies/7.sql
+++ pset7/exercicios/movies/7.sql
@@ -1,5 +1,3 @@
-Em
-7.sql
-, escreva uma consulta SQL para listar todos os filmes lançados em 2010 e suasclassificações, em ordem decrescente por classificação. Para filmes com a mesmaclassificação, ordene-os em ordem alfabética por título.
-Sua consulta deve gerar uma tabela com duas colunas, uma para o título de cada filmee outra para a classificação de cada filme.
-Filmes sem classificação não devem ser incluídos no resultado.
+-- Em 7.sql, escreva uma consulta SQL para listar todos os filmes lançados em 2010 e suasclassificações, em ordem decrescente por classificação. Para filmes com a mesmaclassificação, ordene-os em ordem alfabética por título.
+-- Sua consulta deve gerar uma tabela com duas colunas, uma para o título de cada filmee outra para a classificação de cada filme.
+-- Filmes sem classificação não devem ser incluídos no resultado.
