cdae5c1ebb5303853830f7f8c15b38f2a542ba17
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-01 04:32:37 +0000

    Sat, Apr 1, 2023, 4:32 AM +00:00

--- pset7/exercicios/movies/7.sql
+++ pset7/exercicios/movies/7.sql
@@ -6,4 +6,4 @@ SELECT movies.title, ratings.rating FROM movies
 INNER JOIN ratings
 ON movies.id = ratings.movie_id
 WHERE movies.year = 2010
-ORDER BY ratings.rating AND movies.title DESC;
+ORDER BY ratings.rating, movies.title DESC;
