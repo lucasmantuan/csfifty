6d5d7cfb0a507b09d25834c9cc0ab49244dbece6
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-03 01:03:16 +0000

    Mon, Apr 3, 2023, 1:03 AM +00:00

---
+++ pset7/exercicios/movies/10.sql
@@ -0,0 +1,9 @@
+Em 10.sql, escreva uma consulta SQL para listar os nomes de todas as pessoas que dirigiram um filme que recebeu uma classificação de pelo menos 9,0.
+Sua consulta deve gerar uma tabela com uma única coluna para o nome de cada pessoa.
+Se uma pessoa dirigiu mais de um filme que recebeu uma classificação de pelo menos 9,0, eles só devem aparecer em seus resultados uma vez.
+
+SELECT DISTINCT people.name FROM people
+INNER JOIN stars ON people.id = stars.person_id
+INNER JOIN movies ON movies.id = stars.movie_id
+WHERE movies.year = 2004
+ORDER BY people.birth;
