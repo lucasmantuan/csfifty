4739a758cc6778068b2ade0af9d2ef72a9b3cab8
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-03 01:38:48 +0000

    Mon, Apr 3, 2023, 1:38 AM +00:00

--- pset7/exercicios/movies/12.sql
+++ pset7/exercicios/movies/12.sql
@@ -3,7 +3,8 @@
 -- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Johnny Depp.
 -- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Helena Bonham Carter.
 
-SELECT movies.title FROM movies
-INNER JOIN pestars ON stars.person_id = people.id
-INNER JOIN movies ON stars.movie_id = movies.id
-LIMIT 5;
+SELECT people.name, movies.title FROM movies
+INNER JOIN stars ON stars.movie_id = movies.id
+INNER JOIN people ON stars.person_id = people.id
+WHERE people.name = 'Johnny Depp'
+OR people.name = 'Helena Bonham Carter';
