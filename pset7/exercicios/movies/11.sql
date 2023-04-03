9d7d51394f785128b0fa318e38649d97d722d254
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-03 01:29:19 +0000

    Mon, Apr 3, 2023, 1:29 AM +00:00

--- pset7/exercicios/movies/11.sql
+++ pset7/exercicios/movies/11.sql
@@ -2,7 +2,7 @@
 -- Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
 -- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Chadwick Boseman.
 
-SELECT movies.title, ratings.rating FROM people
+SELECT movies.title FROM people
 INNER JOIN stars ON stars.person_id = people.id
 INNER JOIN movies ON stars.movie_id = movies.id
 INNER JOIN ratings ON ratings.movie_id = movies.id
--- pset7/exercicios/movies/12.sql
+++ pset7/exercicios/movies/12.sql
@@ -2,3 +2,10 @@
 -- Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
 -- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Johnny Depp.
 -- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Helena Bonham Carter.
+
+SELECT movies.title FROM people
+INNER JOIN stars ON stars.person_id = people.id
+INNER JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = 'Johnny Depp'
+ORDER BY ratings.rating DESC
+LIMIT 5;
