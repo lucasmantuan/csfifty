38a39ed3073d24181a0b63b83df3ed8b420a99bb
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-03 01:27:52 +0000

    Mon, Apr 3, 2023, 1:27 AM +00:00

--- pset7/exercicios/movies/11.sql
+++ pset7/exercicios/movies/11.sql
@@ -2,8 +2,10 @@
 -- Sua consulta deve gerar uma tabela com uma única coluna para o título de cada filme.
 -- Você pode presumir que há apenas uma pessoa no banco de dados com o nome Chadwick Boseman.
 
-SELECT people.name FROM people
+SELECT movies.title, ratings.rating FROM people
 INNER JOIN stars ON stars.person_id = people.id
 INNER JOIN movies ON stars.movie_id = movies.id
 INNER JOIN ratings ON ratings.movie_id = movies.id
-WHERE ratings.rating >= 9;
+WHERE people.name = 'Chadwick Boseman'
+ORDER BY ratings.rating DESC
+LIMIT 5;
