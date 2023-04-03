f4fbb195425f688ca3890a7a479d7d4140aaaf65
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-03 01:29:55 +0000

    Mon, Apr 3, 2023, 1:29 AM +00:00

--- pset7/exercicios/movies/12.sql
+++ pset7/exercicios/movies/12.sql
@@ -6,6 +6,4 @@
 SELECT movies.title FROM people
 INNER JOIN stars ON stars.person_id = people.id
 INNER JOIN movies ON stars.movie_id = movies.id
-WHERE people.name = 'Johnny Depp'
-ORDER BY ratings.rating DESC
-LIMIT 5;
+WHERE people.name = 'Johnny Depp';
