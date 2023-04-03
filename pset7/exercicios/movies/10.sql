55f29786a3617d6f1b19cd094ac9b3d6c39e1f84
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-03 01:18:21 +0000

    Mon, Apr 3, 2023, 1:18 AM +00:00

--- pset7/exercicios/movies/10.sql
+++ pset7/exercicios/movies/10.sql
@@ -4,4 +4,8 @@
 
 SELECT people.name FROM people
 INNER JOIN directors ON directors.person_id = people.id;
-INNER JOIN movies ON movies.id = stars.movie_id;
+
+INNER JOIN movies ON movies.id
+SELECT movies.title, ratings.rating FROM movies
+INNER JOIN ratings ON ratings.movie_id = movies.id
+WHERE ratings.rating >= 9;
