478ee6fa91c47963df54abcd862dc031fb75560e
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-11 19:32:59 +0000

    Tue, Apr 11, 2023, 7:32 PM +00:00

--- pset7/exercicios/fiftyville/log.sql
+++ pset7/exercicios/fiftyville/log.sql
@@ -10,4 +10,11 @@ FROM interviews
 WHERE month = 7 AND day = 28
 AND transcript LIKE '%courthouse%';
 
-SELECT 
+SELECT courthouse_security_logs.activity, courthouse_security_logs.license_plate, people.name
+FROM people
+JOIN courthouse_security_logs ON courthouse_security_logs.license_plate = people.license_plate
+WHERE courthouse_security_logs.month = 7
+AND courthouse_security_logs.day = 28
+AND courthouse_security_logs.hour = 10
+AND courthouse_security_logs.minute >= 15
+AND courthouse_security_logs.minute <= 25;
