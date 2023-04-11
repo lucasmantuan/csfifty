8fa9bb2753d3406cd09ceb9207e8aa82608b9786
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-11 20:36:31 +0000

    Tue, Apr 11, 2023, 8:36 PM +00:00

--- pset7/exercicios/fiftyville/log.sql
+++ pset7/exercicios/fiftyville/log.sql
@@ -75,4 +75,5 @@ SELECT name
 FROM people
 JOIN passengers ON people.passport_number = passengers.passport_number
 JOIN flights ON flights.id = passengers.flight_id
-WHERE (flights.month = 7 )
+WHERE (flights.month = 7 AND flights.day = 29 AND flights.id = 36)
+AND name IN ()
