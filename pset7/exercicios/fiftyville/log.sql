2c2b47453f5442303eeb131636634c34304b2ab0
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-11 18:16:39 +0000

    Tue, Apr 11, 2023, 6:16 PM +00:00

--- pset7/exercicios/fiftyville/log.sql
+++ pset7/exercicios/fiftyville/log.sql
@@ -1,6 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
-SELECT description
-FROM crime_scene_reports
-WHERE month = 7 AND day = 28
-AND street = 'Chamberlin Street';
+-- SELECT description
+-- FROM crime_scene_reports
+-- WHERE month = 7 AND day = 28
+-- AND street = 'Chamberlin Street';
+
+SELECT * 
