1a1843a68e177f153cf3ff75739a20ac23b639fd
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-11 20:09:46 +0000

    Tue, Apr 11, 2023, 8:09 PM +00:00

--- pset7/exercicios/fiftyville/log.sql
+++ pset7/exercicios/fiftyville/log.sql
@@ -1,25 +1,25 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- SELECT description
--- FROM crime_scene_reports
--- WHERE month = 7 AND day = 28
--- AND street = 'Chamberlin Street';
+SELECT description
+FROM crime_scene_reports
+WHERE month = 7 AND day = 28
+AND street = 'Chamberlin Street';
 
--- SELECT transcript
--- FROM interviews
--- WHERE month = 7 AND day = 28
--- AND transcript LIKE '%courthouse%';
+SELECT transcript
+FROM interviews
+WHERE month = 7 AND day = 28
+AND transcript LIKE '%courthouse%';
 
--- SELECT courthouse_security_logs.activity, courthouse_security_logs.license_plate, people.name
--- FROM people
--- JOIN courthouse_security_logs ON courthouse_security_logs.license_plate = people.license_plate
--- WHERE courthouse_security_logs.month = 7
--- AND courthouse_security_logs.day = 28
--- AND courthouse_security_logs.hour = 10
--- AND courthouse_security_logs.minute >= 15
--- AND courthouse_security_logs.minute <= 25;
+SELECT courthouse_security_logs.activity, courthouse_security_logs.license_plate, people.name
+FROM people
+JOIN courthouse_security_logs ON courthouse_security_logs.license_plate = people.license_plate
+WHERE courthouse_security_logs.month = 7
+AND courthouse_security_logs.day = 28
+AND courthouse_security_logs.hour = 10
+AND courthouse_security_logs.minute >= 15
+AND courthouse_security_logs.minute <= 25;
 
-SELECT people.name, atm_transactions.transaction_type, atm_transactions.atm_location
+SELECT people.name, atm_transactions.transaction_type
 FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
