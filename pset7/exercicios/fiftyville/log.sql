70d2930fd095530c6ea58dcf7a65d83b040921a0
Lucas Mantuan <lucasmantuan@outlook.com>
2023-04-11 20:07:46 +0000

    Tue, Apr 11, 2023, 8:07 PM +00:00

--- pset7/exercicios/fiftyville/log.sql
+++ pset7/exercicios/fiftyville/log.sql
@@ -19,9 +19,11 @@
 -- AND courthouse_security_logs.minute >= 15
 -- AND courthouse_security_logs.minute <= 25;
 
-SELECT people.name, atm_transactions.transaction_type
+SELECT people.name, atm_transactions.transaction_type, atm_transactions.atm_location
 FROM people
 JOIN bank_accounts ON bank_accounts.person_id = people.id
 JOIN atm_transactions ON atm_transactions.account_number = bank_accounts.account_number
-WHERE atm_transactions.atm_location = 'Fifer Street'
-AND atm_transactions.transaction_type = 'withdrawing';
+WHERE atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_transactions.atm_location = 'Fifer Street'
+AND atm_transactions.transaction_type = 'withdraw';
